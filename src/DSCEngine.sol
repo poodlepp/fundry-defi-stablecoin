e667a94764eba71f7ff821e923c891ab08143fc0
pengyi1 <pengyi1@staff.weibo.com>
2024-05-11 21:04:48 +0800

    add: deposit merge mint

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -121,6 +121,14 @@ contract DSCEngine is ReentrancyGuard {
     //////////////////
     //  External Functions
     //////////////////
+
+    /**
+     * 
+     * @param tokenCollateralAddress: The ERC20 token address of the collateral you're depositing
+     * @param amountCollateral: The amount of collateral you're depositing
+     * @param amountDscToMint: The amount of DSC you want to mint
+     * @notice This function will deposit your collateral and mint DSC in one transaction
+     */
     function depositCollateralAndMintDsc(
         address tokenCollateralAddress,
         uint256 amountCollateral,
